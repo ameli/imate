12fdf80e2df188ebdf4f31ebfbd89e0db486fd19
Siavash Ameli <sia.sunrise@gmail.com>
2020-12-20 01:20:17 -0800

    build wheels

---
+++ .github/build_wheel.sh
@@ -0,0 +1,8 @@
+set -e
+set -x
+
+cd /io
+/opt/python/cp37-cp37m/bin/python setup.py bdist_wheel
+/opt/python/cp38-cp38/bin/python setup.py bdist_wheel
+cd dist
+ls *.whl | xargs -L1 auditwheel repair 
