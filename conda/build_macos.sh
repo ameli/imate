ab5bb098430c847522a325660403d689a41e9b72
Siavash Ameli <sia.sunrise@gmail.com>
2020-12-27 13:54:53 -0800

    wheel

--- conda/build_macos.sh
+++ conda/build_macos.sh
@@ -4,15 +4,16 @@ set -x
 
 # brew install libomp
 # export CFLAGS="${CFLAGS} -isysroot=/Library/Developer/CommandLineTools/SDKs/MacOSX10.15.sdk -rpath ${PREFIX}/lib -L${PREFIX}/lib"
-export CFLAGS="-isysroot=/Library/Developer/CommandLineTools/SDKs/MacOSX10.15.sdk -rpath ${PREFIX}/lib -L${PREFIX}/lib"
-export SDKROOT="/Library/Developer/CommandLineTools/SDKs/MacOSX10.15.sdk"
-echo "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"
-echo $CONDA_BUILD_SYSROOT
-export CONDA_BUILD_SYSROOT="/Library/Developer/CommandLineTools/SDKs/MacOSX10.15.sdk"
-echo $CONDA_BUILD_SYSROOT
-echo "BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB"
+# export CFLAGS="-isysroot=/Library/Developer/CommandLineTools/SDKs/MacOSX10.15.sdk -rpath ${PREFIX}/lib -L${PREFIX}/lib"
+# export SDKROOT="/Library/Developer/CommandLineTools/SDKs/MacOSX10.15.sdk"
+# echo "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"
+# echo $CONDA_BUILD_SYSROOT
+# export CONDA_BUILD_SYSROOT="/Library/Developer/CommandLineTools/SDKs/MacOSX10.15.sdk"
+# echo $CONDA_BUILD_SYSROOT
+# echo "BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB"
 
-{{ PYTHON }} setup.py install --single-version-externally-managed --record=record.txt -vvv
+# {{ PYTHON }} setup.py install --single-version-externally-managed --record=record.txt -vvv
+{{ PYTHON }} -m pip install --no-deps dist/*.whl
 
 # - export CFLAGS="${CFLAGS} -isysroot ${CONDA_BUILD_SYSROOT}"; {{ PYTHON }} setup.py install --single-version-externally-managed --record=record.txt
 # - python setup.py install
