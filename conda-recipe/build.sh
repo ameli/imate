ff41e4b8527daa7318b6ff629d53b18fd139af2a
Siavash Ameli <sia.sunrise@gmail.com>
2021-07-02 20:45:34 -0700

    build.sh multi wheel

--- conda-recipe/build.sh
+++ conda-recipe/build.sh
@@ -1,23 +1,44 @@
 #! /bin/bash
 
+set -e
 set -x
 
-wheel_installed=false;
-echo $wheel_installed;
-for filename in dist/*.whl;
-do
-    if [ "$wheel_installed" != true ];
-    then
-        echo "Try installing ${filename} ...";
-        python -m pip install --no-deps --force-reinstall $filename 2> /dev/null;
-        error_code=$?;
+# Get python version
+python_version_major=`python -c 'import sys; print(sys.version_info.major)'`
+python_version_minor=`python -c 'import sys; print(sys.version_info.minor)'`
+python_version="${python_version_major}${python_version_minor}"
 
+# Detect operating system
+os=`uname`;
+case "$os" in
+    *"Linux"*) platform="linux";;
+    *"Darwin"*) platform="macosx";;
+    *) echo "Invalid operating system. os: $os."; exit 1;;
+esac
+
+# Iterate throug all wheel files in /dist/* directory.
+wheel_installed=false;
+for wheel_filename in dist/*.whl;
+do
+    # Check if wheel filename matches python version and platform
+    if echo $wheel_filename | grep $python_version | grep $platform; then
+
+        # Matched. Install with pip
+        echo "Try installing wheel file '${wheel_filename}'.";
+        python -m pip install --no-deps --force-reinstall $wheel_filename;
+
+        # Check last error code
+        error_code=$?;
         if [ "${error_code}" -eq 0 ];
         then
+            echo "Wheel '${wheel_filename}' installed successfully.";
             wheel_installed=true;
-            echo "Wheel ${filename} installed successfully.";
-            break
+            break;
         fi
+    else
+        # Did not match. Skip installing this wheel file.
+        echo -n "Wheel file '${wheel_filename}' didn't match python version: ";
+        echo "'${python_version}' and platform: '${platform}'. Skipping.";
     fi
 done
 
