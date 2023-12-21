77d7bdb7b402a1c0487686e50e05fa868bff5dd6
Siavash Ameli <sia.sunrise@gmail.com>
2023-12-20 16:51:45 -0800

    pypy 3

--- setup.py
+++ setup.py
@@ -1650,7 +1650,7 @@ def main(argv):
         ),
         ext_modules=external_modules,
         install_requires=requirements,
-        python_requires='>=3.9',
+        python_requires='>=3.8',
         setup_requires=[
             'setuptools',
             'wheel',
